889ec26cdc84f26ce516a38a76db30bad49e9719
UrielTreleani <urieltreleani2002@gmail.com>
2025-11-09 17:27:20 -0300

    PORTFOLIO - 2

--- src/Components/ContactSection/ContactSection.jsx
+++ src/Components/ContactSection/ContactSection.jsx
@@ -8,11 +8,11 @@ function ContactSection() {
         Contacto
       </h2>
       <div>
-        <span>
+        <span className='contact-section__contact-details'>
           <i class="bi bi-envelope"></i>
           Email: urieltreleani2002@gmail.com
         </span>
-        <span>
+        <span className='contact-section__contact-details'>
           <a href="https://github.com/UrielTreleani?tab=repositories" target="_blank">
             <i class="bi bi-github"></i>
             Github
