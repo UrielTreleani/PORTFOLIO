2ca365ce3ad57b81bc7adb0393c6566ad0533fc1
UrielTreleani <urieltreleani2002@gmail.com>
2025-12-21 19:31:44 -0300

    mobile

--- src/Components/MobileMenu/MobileMenu.jsx
+++ src/Components/MobileMenu/MobileMenu.jsx
@@ -11,7 +11,7 @@ function MobileMenu() {
     return (
         <div className="mobile-menu__container">
             <div className="mobile-menu__header">
-                <img src="/images/navbar-icon.png" alt="portfolio-icon" className= "mobile-menu__icon" />
+                <img src="/images/navbar-icon.png" alt="portfolio-icon" className="mobile-menu__icon" />
                 <button onClick={toggleMenu} className="mobile-menu__button">
                     <i class="bi bi-x-lg"></i>
                 </button>
@@ -25,14 +25,14 @@ function MobileMenu() {
                         </a>
                     )
                 })}
-            </div>
-            <div className="mobile-menu__links-container">
-                <a href="https://www.linkedin.com/in/urieltreleani" target="_blank">
-                    <i className="bi bi-linkedin mobile-menu__link"></i>
-                </a>
-                <a href="https://github.com/UrielTreleani?tab=repositories" target="_blank">
-                    <i className="bi bi-github mobile-menu__link"></i>
-                </a>
+                <div className="mobile-menu__links-container">
+                    <a href="https://www.linkedin.com/in/urieltreleani" target="_blank">
+                        <i className="bi bi-linkedin mobile-menu__link"></i>
+                    </a>
+                    <a href="https://github.com/UrielTreleani?tab=repositories" target="_blank">
+                        <i className="bi bi-github mobile-menu__link"></i>
+                    </a>
+                </div>
             </div>
         </div>
     )
